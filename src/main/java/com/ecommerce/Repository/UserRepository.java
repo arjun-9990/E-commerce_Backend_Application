3a481a7f367ea100f72aefd4ef13ca602d6d43ce
Arjun Gaikwad <arjungaikwad9320@gmail.com>
2025-11-24 00:37:14 +0530

    User Repository

---
+++ src/main/java/com/ecommerce/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.Repository;
+
+import com.ecommerce.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUserName(String username);
+}
