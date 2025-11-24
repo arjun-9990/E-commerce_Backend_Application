4b0a63679780fda9e40e9e8f77a1c6704ae10423
Arjun Gaikwad <arjungaikwad9320@gmail.com>
2025-11-24 09:35:54 +0530

    Login And Signup DTOs

--- src/main/java/com/ecommerce/Security/request/LoginRequest.java
+++ src/main/java/com/ecommerce/Security/request/LoginRequest.java
@@ -1,8 +1,12 @@
-package com.ecommerce.Security.Jwt;
+package com.ecommerce.Security.request;
+
+import jakarta.validation.constraints.NotBlank;
 
 public class LoginRequest {
+    @NotBlank
     private String username;
 
+    @NotBlank
     private String password;
 
     public String getUsername() {
