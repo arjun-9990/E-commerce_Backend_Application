8d4021f632e977feb27b6fd82fe9a01db211399d
Arjun Gaikwad <arjungaikwad9320@gmail.com>
2025-11-25 12:10:44 +0530

    get Clean JwtCookie

--- src/main/java/com/ecommerce/Security/Jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/Security/Jwt/JwtUtils.java
@@ -71,6 +71,15 @@ public class JwtUtils {
         return cookie;
     }
 
+    public ResponseCookie getCleanJwtCookie(){
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie,null)
+                .path("/api")
+                .build();
+
+        return cookie;
+    }
+
+
     public String generateTokenFromUsername(String username) {
         return Jwts.builder()
                 .subject(username)
