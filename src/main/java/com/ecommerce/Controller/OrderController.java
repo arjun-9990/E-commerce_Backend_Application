4fd4cfad50ae3ee6ecc5a86c49bd2c8beda40089
Arjun Gaikwad <arjungaikwad9320@gmail.com>
2025-11-28 17:07:40 +0530

    Creating Order Controller

---
+++ src/main/java/com/ecommerce/Controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.ecommerce.Controller;
+
+import com.ecommerce.Payload.OrderDTO;
+import com.ecommerce.Payload.OrderRequestDTO;
+import com.ecommerce.Service.OrderService;
+import com.ecommerce.Util.AuthUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @PostMapping("/order/users/payments/{paymentMethod}")
+    public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod, @RequestBody OrderRequestDTO orderRequestDTO) {
+        String emailId = authUtil.loggedInEmail();
+        OrderDTO order = orderService.placeOrder(
+                emailId,
+                orderRequestDTO.getAddressId(),
+                paymentMethod,
+                orderRequestDTO.getPgName(),
+                orderRequestDTO.getPgPaymentId(),
+                orderRequestDTO.getPgStatus(),
+                orderRequestDTO.getPgResponseMessage()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    }
+}
