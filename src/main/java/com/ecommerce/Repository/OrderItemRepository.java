8dd8c1186fdf9aaa36e5d02e9ce7be154dab79ee
Arjun Gaikwad <arjungaikwad9320@gmail.com>
2025-11-28 17:06:41 +0530

    Created OrderItemRepository

---
+++ src/main/java/com/ecommerce/Repository/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.Repository;
+
+import com.ecommerce.Model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
