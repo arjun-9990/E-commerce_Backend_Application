d9fda946552d6c002ed8c04bdbc626548b5c57a2
Arjun Gaikwad <arjungaikwad9320@gmail.com>
2025-11-28 11:25:55 +0530

    add repository

---
+++ src/main/java/com/ecommerce/Repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.Repository;
+
+import com.ecommerce.Model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address,Long> {
+}
