451a52db7ce657e98ff93038c14c0f01acdaa027
Arjun Gaikwad <arjungaikwad9320@gmail.com>
2025-11-28 17:07:53 +0530

    Creating Order service

---
+++ src/main/java/com/ecommerce/Service/OrderService.java
@@ -0,0 +1,9 @@
+package com.ecommerce.Service;
+
+import com.ecommerce.Payload.OrderDTO;
+import jakarta.transaction.Transactional;
+
+public interface OrderService {
+    @Transactional
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
